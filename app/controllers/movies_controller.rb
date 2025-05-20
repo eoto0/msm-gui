04167a988fad41ff5c179d9671017c9667fe5f3c
eoto0 <eoto0@chicagobooth.edu>
2025-05-20 22:02:25 +0000

    Added

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,30 @@
 class MoviesController < ApplicationController
+  def create
+
+    #params hash looks like this
+    #{"the_title"=>"1", "the_year"=>"3", "the_duration"=>"4", "the_description"=>"5", "the_image"=>"6", "the_director_id"=>"7"}
+
+    m = Movie.new
+    m.title = params.fetch("the_title")
+    m.year = params.fetch("the_year")
+    m.duration = params.fetch("the_duration")
+    m.description = params.fetch("the_description")
+    m.image = params.fetch("the_image")
+    m.director_id = params.fetch("the_director_id")
+
+    m.save
+
+    redirect_to("/movies")
+
+    #Retrieve the user's inputs from params
+    #Create a record in the movie table
+    #Populate each column with the user input
+    #Save
+
+
+  end
+  
+  
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
